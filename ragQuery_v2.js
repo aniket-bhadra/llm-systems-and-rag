33e9d78fddbe9211e7c702ed54fc22f7fb0293b8
aniket-bhadra <aniketbhadra98320@gmail.com>
2025-09-05 14:10:21 +0530

    implementd RAG system using Pinecone, Gemini API, and LangChain with enhanced query mechanism

---
+++ ragQuery_v2.js
@@ -0,0 +1,98 @@
+import * as dotenv from "dotenv";
+import readlineSync from "readline-sync";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { Pinecone } from "@pinecone-database/pinecone";
+import { GoogleGenAI } from "@google/genai";
+
+dotenv.config();
+const ai = new GoogleGenAI({});
+const History = [];
+
+async function transformQuery(question) {
+  History.push({
+    role: "user",
+    parts: [{ text: question }],
+  });
+
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: History,
+    config: {
+      systemInstruction: `You are a query rewriting expert. Based on the provided chat history, rephrase the "Follow Up user Question" into a complete, standalone question that can be understood without the chat history.
+    Only output the rewritten question and nothing else.
+      `,
+    },
+  });
+
+  History.pop();
+
+  return response.text;
+}
+
+async function chatting(question) {
+  const query = await transformQuery(question);
+  //convert the Q into vector
+  const embeddings = new GoogleGenerativeAIEmbeddings({
+    apiKey: process.env.GEMINI_API_KEY,
+    model: "text-embedding-004",
+  });
+
+  //since here we have 1 Q to convert to vector, instead of using langchain directly here we embedded, if there are multiples then we must use langchian or for loop
+  const queryVector = await embeddings.embedQuery(query);
+
+  //make connection with pinecone
+  const pinecone = new Pinecone();
+  const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+
+  const searchResults = await pineconeIndex.query({
+    topK: 10,
+    vector: queryVector,
+    includeMetadata: true,
+  });
+
+//   console.log(searchResults.length);
+
+  //crate the context for llm
+  const context = searchResults.matches
+    .map((match) => match.metadata.text)
+    .join("\n\n---\n\n");
+
+//   console.log(context);
+
+  History.push({
+    role: "user",
+    parts: [{ text: query }],
+  });
+
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: History,
+    config: {
+      systemInstruction: `You have to behave like a Graphql API expert.
+    You will be given a context of relevant information and a user question.
+    Your task is to answer the user's question based ONLY on the provided context.
+    If the answer is not in the context, you must say "I could not find the answer in the provided document."
+    Keep your answers clear, concise, and educational.
+      
+      Context: ${context}
+      `,
+    },
+  });
+
+  History.push({
+    role: "model",
+    parts: [{ text: response.text }],
+  });
+
+  console.log("\n");
+  console.log(response.text);
+}
+
+async function main() {
+  while (true) {
+    const userProblem = readlineSync.question("Ask me anything--> ");
+    await chatting(userProblem);
+  }
+}
+
+main();
