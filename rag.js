d29a999f05d5dd7f0e345e13821650f5d52a955d
aniket-bhadra <aniketbhadra98320@gmail.com>
2025-09-05 11:57:55 +0530

    phase-1 in RAG implementation pdf loading,chunking,vector embedding configuration, db configuration, saving to db completed

---
+++ rag.js
@@ -0,0 +1,50 @@
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { Pinecone } from "@pinecone-database/pinecone";
+import { PineconeStore } from "@langchain/pinecone";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+async function indexDocument() {
+  //load the pdf
+  const PDF_PATH = "./graphql.pdf";
+  const pdfLoader = new PDFLoader(PDF_PATH);
+  const rawDocs = await pdfLoader.load();
+  //   console.log(rawDocs.length);
+
+  console.log("pdf loaded");
+
+  //chunking
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+  });
+  const chunkedDocs = await textSplitter.splitDocuments(rawDocs);
+
+  console.log("chunking done");
+
+  //vector embedding model configure
+  const embeddings = new GoogleGenerativeAIEmbeddings({
+    apiKey: process.env.GEMINI_API_KEY,
+    model: "text-embedding-004",
+  });
+
+  console.log("vector embedding model configured");
+  //database configure
+  const pinecone = new Pinecone();
+  const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+
+  //langchain takes the chunking docs,embedding model, and after embedding which db we need to store, langchain does all this in single step
+  console.log("pinecone configured");
+
+  await PineconeStore.fromDocuments(chunkedDocs, embeddings, {
+    pineconeIndex,
+    maxConcurrency: 5,
+  });
+  console.log("saved to pinecone");
+}
+
+indexDocument();
+
